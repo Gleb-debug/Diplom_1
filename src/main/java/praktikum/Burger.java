2346585c8ae09facb01ad992029c0e8492cb2847
Gleb-debug <soloviov.glebich@yandex.ru>
2025-05-10 22:38:12 +0500

    Тесты

---
+++ src/main/java/praktikum/Burger.java
@@ -0,0 +1,62 @@
+package praktikum;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Модель бургера.
+ * Бургер состоит из булочек и ингредиентов (начинка или соус).
+ * Ингредиенты можно перемещать и удалять.
+ * Можно распечать чек с информацией о бургере.
+ */
+public class Burger {
+
+    public Bun bun;
+    public List<Ingredient> ingredients = new ArrayList<>();
+
+    public void setBuns(Bun bun) {
+        this.bun = bun;
+    }
+
+    public void addIngredient(Ingredient ingredient) {
+        ingredients.add(ingredient);
+    }
+
+    public void removeIngredient(int index) {
+        ingredients.remove(index);
+    }
+
+    public void moveIngredient(int index, int newIndex) {
+        ingredients.add(newIndex, ingredients.remove(index));
+    }
+
+    public float getPrice() {
+        float price = bun.getPrice() * 2;
+
+        for (Ingredient ingredient : ingredients) {
+            price += ingredient.getPrice();
+        }
+
+        return price;
+    }
+
+    public String getReceipt() {
+        StringBuilder receipt = new StringBuilder(String.format("(==== %s ====)\n", bun.getName()));
+
+        for (Ingredient ingredient : ingredients) {
+            // Проверка типа ингредиента и вывод на русском
+            String typeInRussian;
+            if (ingredient.getType() == IngredientType.SAUCE) {
+                typeInRussian = "Соус:"; // Для соусов
+            } else {
+                typeInRussian = "Начинка:"; // Для начинок
+            }
+            receipt.append(String.format("= %s %s =\n", typeInRussian, ingredient.getName()));
+        }
+
+        receipt.append(String.format("(==== %s ====)\n", bun.getName()));
+        receipt.append(String.format("\nЦена: %.2f", getPrice()));
+
+        return receipt.toString();
+    }
+}
---
+++ src/main/java/praktikum/Database.java
@@ -0,0 +1,42 @@
+package praktikum;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Класс с методами по работе с базой данных.
+ */
+public class Database {
+
+    private final List<Bun> buns = new ArrayList<>();
+    private final List<Ingredient> ingredients = new ArrayList<>();
+
+    public Database() {
+        buns.add(new Bun("Флюоресцентная булка R2-D3", 988));
+        buns.add(new Bun("Краторная булка N-200i", 1255));
+
+        ingredients.add(new Ingredient(IngredientType.SAUCE, "Spicy-X", 90));
+        ingredients.add(new Ingredient(IngredientType.SAUCE, "Фирменный Space Sauce", 80));
+        ingredients.add(new Ingredient(IngredientType.SAUCE, "Традиционный галактический", 15));
+        ingredients.add(new Ingredient(IngredientType.SAUCE, "С шипами Антарианского плоскоходца", 88));
+
+        ingredients.add(new Ingredient(IngredientType.FILLING, "Мясо бессмертных моллюсков Protostomia", 1337));
+        ingredients.add(new Ingredient(IngredientType.FILLING, "Говяжий метеорит (отбивная)", 3000));
+        ingredients.add(new Ingredient(IngredientType.FILLING, "Биокотлета из марсианской Магнолии", 424));
+        ingredients.add(new Ingredient(IngredientType.FILLING, "Филе Люминесцентного тетраодонтимформа", 988));
+        ingredients.add(new Ingredient(IngredientType.FILLING, "Хрустящие минеральные кольца", 300));
+        ingredients.add(new Ingredient(IngredientType.FILLING, "Плоды Фалленианского дерева", 874));
+        ingredients.add(new Ingredient(IngredientType.FILLING, "Кристаллы марсианских альфа-сахаридов", 762));
+        ingredients.add(new Ingredient(IngredientType.FILLING, "Мини-салат Экзо-Плантаго", 4400));
+        ingredients.add(new Ingredient(IngredientType.FILLING, "Сыр с астероидной плесенью", 4142));
+    }
+
+    public List<Bun> availableBuns() {
+        return buns;
+    }
+
+    public List<Ingredient> availableIngredients() {
+        return ingredients;
+    }
+
+}
